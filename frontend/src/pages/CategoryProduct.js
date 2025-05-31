3eb5ca52b3f15e8666c05401f307201e3e1c4000
Shivateja Modugapalam <157293676+M-Shivateja@users.noreply.github.com>
2025-06-01 01:35:32 +0530

    Update CategoryProduct.js

--- frontend/src/pages/CategoryProduct.js
+++ frontend/src/pages/CategoryProduct.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../components/Layout/Layout";
 import { useParams, useNavigate } from "react-router-dom";
-import axios from "axios";
+import axios from "../config/axios";
 import { useCart } from "../context/cart";
 import toast from "react-hot-toast";
 import { FiArrowLeft, FiShoppingCart } from "react-icons/fi";
