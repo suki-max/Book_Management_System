88892a22e50484ddc23e4acd82f318e4713d0c09
Shivateja Modugapalam <157293676+M-Shivateja@users.noreply.github.com>
2025-06-01 01:43:54 +0530

    Update UpdateProduct.js

--- frontend/src/pages/Admin/UpdateProduct.js
+++ frontend/src/pages/Admin/UpdateProduct.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Layout from "./../../components/Layout/Layout";
 import AdminMenu from "./../../components/Layout/AdminMenu";
 import toast from "react-hot-toast";
-import axios from "axios";
+import axios from "../../config/axios";
 import { Select } from "antd";
 import { useNavigate, useParams } from "react-router-dom";
 const { Option } = Select;
