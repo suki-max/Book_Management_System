5ce99b551f086a8a86ab0b46a3a0c4faed9bbabc
Shivateja Modugapalam <157293676+M-Shivateja@users.noreply.github.com>
2025-06-01 00:15:16 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,12 @@ connectDB();
 const app = express();
 
 //middelwares
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
