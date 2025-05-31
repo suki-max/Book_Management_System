778a386292a107b91907ad23f07790d770f14076
Shivateja Modugapalam <157293676+M-Shivateja@users.noreply.github.com>
2025-06-01 01:43:38 +0530

    Update Products.js

--- frontend/src/pages/Admin/Products.js
+++ frontend/src/pages/Admin/Products.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import Layout from "./../../components/Layout/Layout";
-import axios from "axios";
+import axios from "../../config/axios";
 import toast from "react-hot-toast";
 import { Link } from "react-router-dom";
 const Products = () => {
