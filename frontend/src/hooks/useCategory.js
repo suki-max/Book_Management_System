3682036f29e2d4070269e6a79e3db73343bc724e
Shivateja Modugapalam <157293676+M-Shivateja@users.noreply.github.com>
2025-06-01 01:54:44 +0530

    Update useCategory.js

--- frontend/src/hooks/useCategory.js
+++ frontend/src/hooks/useCategory.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import axios from "../config/axios";
 
 export default function useCategory() {
   const [categories, setCategories] = useState([]);
