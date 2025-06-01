6c6102de857681300e54fa86493508501fb62b7d
Shivateja Modugapalam <157293676+M-Shivateja@users.noreply.github.com>
2025-06-01 10:59:02 +0530

    Update Private.js

--- frontend/src/components/Routes/Private.js
+++ frontend/src/components/Routes/Private.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../../context/auth";
 import { Outlet } from "react-router-dom";
-import axios from "axios";
+import axios from "../config/axios.js"
 import Spinner from "../Spinner";
 
 export default function PrivateRoute() {
