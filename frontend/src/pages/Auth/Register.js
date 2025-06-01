c57916ffdc6e5872a194918cb3a6cdff21128291
Shivateja Modugapalam <157293676+M-Shivateja@users.noreply.github.com>
2025-06-01 09:57:08 +0530

    Update Register.js

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Layout from "./../../components/Layout/Layout";
-import axios from "axios";
+import axios from "../../config/axios.js";
 import { useNavigate, Link } from "react-router-dom";
 import toast from "react-hot-toast";
 import {
